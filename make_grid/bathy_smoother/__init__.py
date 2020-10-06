ba31d1823a691f52b77e02f478c6a4f7945fcad5
fernandotcbarreto <fernandotcbarreto@gmail.com>
2020-10-06 08:48:22 -0300

    Add files via upload

---
+++ make_grid/bathy_smoother/__init__.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+'''
+bathy_smoother is a toolkit for working with ROMS bathymetry
+(ripped from matlab script LP_bathymetry)
+'''
+
+from . import bathy_smoothing
+from . import bathy_tools
+from . import LP_bathy_smoothing
+from . import LP_bathy_tools
+from . import LP_tools
+
+__authors__ = ['Frederic Castruccio <frederic@marine.rutgers.edu>']
+
+__version__ = '0.2.0'
